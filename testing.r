645a172423d13d32d864675e96f403306549e491
Shahid <methoomirza@gmail.com>
2021-01-04 16:10:58 +0400

    Update from line 4 to 9

--- testing.r
+++ testing.r
@@ -1,2 +1,10 @@
 # This is first R-Proram From GitHub
 print("Hello from GitHub")
+
+gName = "Shahid"
+version = "RStudio in Writting..."
+
+print(gName)
+print("These lines has been written in RStudio")
+
+
